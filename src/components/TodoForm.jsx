e75b6fea71fe4894a229ca0f636887623fcaf532
harshit shah <harshit.shah@codezeros>
2023-12-01 18:06:30 +0530

    todocontext

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { useTodo } from "../contexts/TodoContext";
+
+function TodoForm() {
+  const [todo, setTodo] = useState("");
+  const { addTodo } = useTodo();
+
+
+
+  
+
+  const add = (e) => {
+    e.preventDefault();
+    if (!todo) return;
+    addTodo({ todo, completed: false });
+    setTodo("");
+  };
+
+  return (
+    <form onSubmit={add} className="flex">
+      <input
+        type="text"
+        placeholder="Add Your Tasks..."
+        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+      />
+      <button
+        type="submit"
+        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+      >
+        Add
+      </button>
+      <select className="rounded-r-lg px-3 py-1 bg-blue-400 text-white shrink-0"  
+      >
+          <option value="all">All</option>
+          <option value="completed">Completed</option>
+          <option value="uncomplete">Uncomplete</option>
+        </select>
+
+
+    </form>
+  );
+}
+
+export default TodoForm;
